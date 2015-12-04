24c4440a79542dd1a855a9dc9ff83f8670d00868
Klint Tiley <kctiley@gmail.com>
2015-12-03 20:39:07 -0700

    Posts showing

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,46 @@
 
 app.controller('myController', function($scope) {
   $scope.greeting = "myController";
+  $scope.showForm = function(){
+    if($scope.displayForm !== true ){
+      $scope.displayForm = true;
+    }
+    else $scope.displayForm = false;
+  }
+
+  $scope.posts = [];
+  $scope.comments = [];
+
+
+  $scope.submit = function(comments){
+    $scope.form = false;
+    $scope.post = {
+      commentsShow: false,
+      commentForm: false,
+      postDate: Date(),
+      title: $scope.title,
+      author: $scope.author,
+      imageURL: $scope.imageURL,
+      description: $scope.description,
+      comments: []
+    }
+    $scope.posts.push($scope.post);
+    $scope.post = {};
+  }
+
+  $scope.showPosts = function(){
+    if($scope.showPosts.length > 0){
+      $scope.displayPosts = true;
+    }
+  }
+
+
+
 });
 
-app.controller('HomeController', function($scope) {
-  $scope.homeMessage = "Home!";
-});
+// app.controller('HomeController', function($scope) {
+//   $scope.homeMessage = "Home!";
+// });
 
 
 // app.controller('ServiceController', function($scope, $http) {
